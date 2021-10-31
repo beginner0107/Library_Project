a2dd5958c2776ab6fbb8bffd85e86b702995f406
seungju17 <dkstmdwn17@naver.com>
2021-10-31 15:46:00 +0900

    도서 서비스 인터페이스

---
+++ src/main/java/kr/green/library/service/BookService.java
@@ -0,0 +1,38 @@
+package kr.green.library.service;
+
+import org.springframework.stereotype.Service;
+
+import kr.green.library.vo.BookImageVO;
+import kr.green.library.vo.BookReplyVO;
+import kr.green.library.vo.BookVO;
+import kr.green.library.vo.CommVO;
+import kr.green.library.vo.PagingVO;
+
+@Service
+public interface BookService {
+	void insert(BookVO bookVO);
+	
+	// 목록 보기
+	PagingVO<BookVO> selectList(CommVO commVO);
+	
+	PagingVO<BookReplyVO> selectReplyList(CommVO commVO);
+
+	BookVO selectByIsbn(String isbn);
+
+	BookVO selectByIsbnTitle(String isbn, String title);
+	// 도서 삭제 
+	void deleteBook(String isbn);
+	// 도서 이미지 삭제 
+	void deleteBookImage(String isbn);
+	// 도서 정보 수정
+	void updateBookInfo(BookVO bookVO);
+	// 도서 정보 수정할 때 이미지 삭제하고 다시 추가 할 때에 수행
+	void insertBookImage(BookImageVO bookImageVO);
+	// 도서 댓글 추가
+	void insertBookReply(BookReplyVO bookReplyVO);
+	// 도서 대여 -> 수량 -1
+	void updateBookCount(String isbn);
+	// 도서 반납 -> 수량 +1;
+	void updateReturnCount(String isbn);
+	
+}
