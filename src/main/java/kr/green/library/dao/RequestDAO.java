7d0846ab0adf1719e70b71d72fdaa038bbdf9ed1
Beginner <81161819+beginner0107@users.noreply.github.com>
2021-11-01 17:40:25 +0900

    request Book insert list
    request Book insert list

--- src/main/java/kr/green/library/dao/RequestDAO.java
+++ src/main/java/kr/green/library/dao/RequestDAO.java
@@ -1,7 +1,14 @@
 package kr.green.library.dao;
 
+import java.util.HashMap;
+import java.util.List;
+
 import kr.green.library.vo.RequestVO;
 
 public interface RequestDAO {
 	void insert(RequestVO requestVO);
+	// 회원 희망도서 개수
+	int selectRequestCount();
+	// 회원 희망 도서 1 페이지 얻기
+	List<RequestVO>selectRequestList(HashMap<String, String>map);
 }
