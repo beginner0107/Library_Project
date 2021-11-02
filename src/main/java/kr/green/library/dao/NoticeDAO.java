b7bcc8fe5c9e359383655247143d664736406969
seungju17 <dkstmdwn17@naver.com>
2021-11-03 01:31:21 +0900

    noticeDAO
    noticeDAO

---
+++ src/main/java/kr/green/library/dao/NoticeDAO.java
@@ -0,0 +1,16 @@
+package kr.green.library.dao;
+
+import java.util.HashMap;
+import java.util.List;
+
+import kr.green.library.vo.NoticeVO;
+
+public interface NoticeDAO {
+	void insert(NoticeVO noticeVO);
+	
+	int selectCount();
+	
+	List<NoticeVO>selectList(HashMap<String, String>map);
+	
+	void delete(int notice_id);
+}
