1b89b82f90058e3cb94e4520da2e19b9dd75bd01
Beginner <81161819+beginner0107@users.noreply.github.com>
2021-11-04 17:56:49 +0900

    create updateReply, deleteReply

--- src/main/java/kr/green/library/dao/BookReplyDAO.java
+++ src/main/java/kr/green/library/dao/BookReplyDAO.java
@@ -11,4 +11,12 @@ public interface BookReplyDAO {
 	List<BookReplyVO> selectReplyList(HashMap<String, String>map);
 	
 	int selectCount(String isbn);
+	
+	void updateReply(BookReplyVO bookReplyVO);
+	
+	void deleteReply(int breply_id);
+	// 댓글 하나 얻기
+	BookReplyVO selectByBreplyId(int breply_id);
+	// 댓글 리스트 전체 읽기
+	List<BookReplyVO> getBookReplyList(String isbn);
 }
