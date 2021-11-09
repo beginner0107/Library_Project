c91dbeb5ab8e7679efe77c9836368259607cb788
Beginner <81161819+beginner0107@users.noreply.github.com>
2021-11-09 17:53:43 +0900

    BookReplyDAO주석 추가
    BookReplyDAO주석 추가

--- src/main/java/kr/green/library/dao/BookReplyDAO.java
+++ src/main/java/kr/green/library/dao/BookReplyDAO.java
@@ -6,17 +6,20 @@ import java.util.List;
 import kr.green.library.vo.BookReplyVO;
 
 public interface BookReplyDAO {
-	int insertBookReply(BookReplyVO bookReplyVO);
-	
-	List<BookReplyVO> selectReplyList(HashMap<String, String>map);
-	
-	int selectCount(String isbn);
-	
-	void updateReply(BookReplyVO bookReplyVO);
-	
-	void deleteReply(int breply_id);
-	// 댓글 하나 얻기
+	// 댓글 하나 얻기 --> 읽기, 수정, 삭제
 	BookReplyVO selectByBreplyId(int breply_id);
-	// 댓글 리스트 전체 읽기
+	// 댓글 저장하기
+	int insertBookReply(BookReplyVO bookReplyVO);
+	// 특정 댓글 수정하기
+	void updateReply(BookReplyVO bookReplyVO);
+	// 특정 댓글 삭제하기
+	void deleteReply(int breply_id);
+	// 전체 댓글 개수 세기
+	int selectCount(String isbn);
+	// 댓글 리스트 Ajax 사용 X 
+	List<BookReplyVO> selectReplyList(HashMap<String, String>map);
+	// 특정 도서의 댓글 리스트 전체 읽기 Ajax 
 	List<BookReplyVO> getBookReplyList(String isbn);
+	// 게시물이 삭제되면 그 게시글의 댓글도 삭제되어야 한다.
+	void deleteAllReply(String isbn);
 }
