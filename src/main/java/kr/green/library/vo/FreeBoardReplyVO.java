18876d877096de0b43ea214388dfc3547338a658
Beginner <81161819+beginner0107@users.noreply.github.com>
2021-11-09 17:51:27 +0900

    날짜 표현에 대해 Jsonformat을 통해 형식지정
    날짜 표현에 대해 Jsonformat을 통해 형식지정

--- src/main/java/kr/green/library/vo/FreeBoardReplyVO.java
+++ src/main/java/kr/green/library/vo/FreeBoardReplyVO.java
@@ -4,6 +4,10 @@ import java.util.Date;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonValueFormat;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,6 +20,7 @@ public class FreeBoardReplyVO {
 	private int fboard_reply_id;
 	private int free_board_id;
 	private String userid;
+	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd hh:mm:ss")
 	private Date fboard_reply_regdate;
 	private String fboard_reply_content;
 }
