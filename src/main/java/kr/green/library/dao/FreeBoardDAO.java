d87368f9204e989b600779f0f9078b48c47e3ccf
Beginner <81161819+beginner0107@users.noreply.github.com>
2021-11-09 17:53:21 +0900

    FreeBoardDAO 주석 추가
    FreeBoardDAO 주석 추가

--- src/main/java/kr/green/library/dao/FreeBoardDAO.java
+++ src/main/java/kr/green/library/dao/FreeBoardDAO.java
@@ -6,22 +6,24 @@ import java.util.List;
 import kr.green.library.vo.FreeBoardVO;
 
 public interface FreeBoardDAO {
-	// 전체 개수 얻기
-	int selectCount();
-	// 1개 얻기(free_board_id를 넘기면 freeBoardVO를 채울 수 있가.)
+	// 게시판 글 한 개 얻기 -> 수정, 삭제를 위해
 	FreeBoardVO selectByBoardId(int free_board_id);
-	// 1페이지 얻기
-	// <!-- 3. 1페이지 얻기 -->
-	List<FreeBoardVO> selectList(HashMap<String, Integer> map);
-	// 저장하기
+	// 게시판 글 쓰기 -> 저장하기
 	void insert(FreeBoardVO freeBoardVO);
-	// 게시글의 고유번호 가지고 와서 수정 / 삭제
+	// 게시글 수정
 	void update(FreeBoardVO freeBoardVO);
+	// 게시글 삭제
 	void delete(int free_board_id);
 	// 현재 저장한 free_board_id값 알아내기
 	int selectSeq();
+	// 전체 개수 얻기
+	int selectCount();
+	// 1페이지 얻기 
+	List<FreeBoardVO> selectList(HashMap<String, Integer> map);
 	int selectAdminCount();
-	// <!-- 3. 1페이지 얻기 -->
+	
+	// --- 관리자 --- 
+	// 
 	List<FreeBoardVO> selectAdminList(HashMap<String, Integer> map);
 	void updateInappropriatePost(FreeBoardVO freeBoardVO);
 }
