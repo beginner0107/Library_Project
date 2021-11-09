a7db1e14a066654c988bc590bca7c97f2f55ab6e
Beginner <81161819+beginner0107@users.noreply.github.com>
2021-11-09 17:54:01 +0900

    BookImageDAO주석 추가
    BookImageDAO주석 추가

--- src/main/java/kr/green/library/dao/BookImageDAO.java
+++ src/main/java/kr/green/library/dao/BookImageDAO.java
@@ -5,10 +5,11 @@ import java.util.List;
 import kr.green.library.vo.BookImageVO;
 
 public interface BookImageDAO {
+	// --- 관리자 ---
+	// 도서 이미지 저장
 	void insert(BookImageVO bookImageVO);
-	
+	// 특정 도서의 이미지 경로들을 List로 받아서 보여줌
 	List<BookImageVO> selectImage(String isbn);
-	
-	//삭제하기
+	// 삭제하기 - 수정할 때 이용 
 	void deleteBookImage(String isbn);
 }
