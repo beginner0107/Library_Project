21c15d461d261c21e5bf3cfb8cb84030f86614d1
seungju17 <dkstmdwn17@naver.com>
2021-11-12 02:01:31 +0900

    대여 서비스 검색 type과 key 값
    대여 서비스 검색 type과 key 값

--- src/main/java/kr/green/library/service/RentServiceImpl.java
+++ src/main/java/kr/green/library/service/RentServiceImpl.java
@@ -37,7 +37,11 @@ public class RentServiceImpl implements RentService{
 		PagingVO<RentVO>pagingVO = null;
 		try {
 			// 전체 개수 구하기
-			int totalCount = rentDAO.rentListCount(userid);
+			HashMap<String, String>totalMap = new HashMap<>();
+			totalMap.put("keyword", commVO.getKeyword());
+			totalMap.put("type", commVO.getType());
+			totalMap.put("userid", userid);
+			int totalCount = rentDAO.rentListCount(totalMap);
 			// 페이지 계산
 			pagingVO = new PagingVO<>(commVO.getCurrentPage(), commVO.getPageSize(), commVO.getBlockSize(), totalCount, commVO.getType(), commVO.getKeyword());
 			// 글을 읽어오기
@@ -45,6 +49,9 @@ public class RentServiceImpl implements RentService{
 			map.put("userid", userid);
 			map.put("startNo", pagingVO.getStartNo()+"");
 			map.put("endNo", pagingVO.getEndNo()+"");
+			map.put("keyword", pagingVO.getKeyword());
+			map.put("type", pagingVO.getType());
+			map.put("userid", userid);
 			List<RentVO> list = rentDAO.rentListByUserid(map);
 			// 완성된 리스트를 페이징 객체에 넣는다.
 			pagingVO.setList(list);
@@ -79,18 +86,25 @@ public class RentServiceImpl implements RentService{
 		return rentDAO.selectReturnAvailable(map);
 	}
 	@Override
-	public PagingVO<RentVO> selectOverdueBookList(CommVO commVO) {
+	public PagingVO<RentVO> selectOverdueBookList(CommVO commVO, String userid) {
 		log.info("{}의 selectList 호출 : {}", this.getClass().getName(), commVO);
 		PagingVO<RentVO>pagingVO = null;
 		try {
+			HashMap<String, String>totalMap = new HashMap<>();
+			totalMap.put("keyword", commVO.getKeyword());
+			totalMap.put("type", commVO.getType());
+			totalMap.put("userid", userid);
 			// 전체 개수 구하기
-			int totalCount = rentDAO.selectOverdueBookCount();
+			int totalCount = rentDAO.selectOverdueBookCount(totalMap);
 			// 페이지 계산
 			pagingVO = new PagingVO<>(commVO.getCurrentPage(), commVO.getPageSize(), commVO.getBlockSize(), totalCount, commVO.getType(), commVO.getKeyword());
 			// 글을 읽어오기
 			HashMap<String, String> map = new HashMap<String, String>();
 			map.put("startNo", pagingVO.getStartNo()+"");
 			map.put("endNo", pagingVO.getEndNo()+"");
+			map.put("type", pagingVO.getType());
+			map.put("keyword", pagingVO.getKeyword());
+			map.put("userid", userid);
 			List<RentVO> list = rentDAO.selectOverdueBookList(map);
 			// 완성된 리스트를 페이징 객체에 넣는다.
 			pagingVO.setList(list);
