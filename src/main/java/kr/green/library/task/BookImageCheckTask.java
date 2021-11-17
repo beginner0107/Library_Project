bcc7319636a418af25b5f0416e015badabee32a2
seungju17 <dkstmdwn17@naver.com>
2021-11-18 01:44:30 +0900

    quartz 주기적으로 이미지 삭제
    quartz 주기적으로 이미지 삭제

---
+++ src/main/java/kr/green/library/task/BookImageCheckTask.java
@@ -0,0 +1,91 @@
+package kr.green.library.task;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import kr.green.library.dao.BookImageDAO;
+import kr.green.library.vo.BookImageVO;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class BookImageCheckTask {
+
+	@Autowired
+	private BookImageDAO bookImageDAO;
+	
+private String getFolderYesterDay() {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		
+		Calendar cal = Calendar.getInstance();
+		
+		cal.add(Calendar.DATE, -1);
+		
+		String str = sdf.format(cal.getTime());
+		
+		return str.replace("-", File.separator);
+	}	
+	
+	@Scheduled(cron="0 0 1 * * *") // 매일 새벽 1시에 동작
+	public void checkFiles() throws Exception{	
+		
+		log.warn("File Check Task Run..........");
+		log.info("new Date : {}", new Date());
+		log.warn("========================================");		
+		
+		// DB에 저장된 파일 리스트
+		List<BookImageVO> fileList = bookImageDAO.checkFileList();		
+		
+		
+		// 비교 기준 파일 리스트(Path객체)
+		List<Path> checkFilePath = new ArrayList<Path>();
+			//원본 이미지
+		fileList.forEach(vo -> {
+			Path path = Paths.get("C:\\upload", vo.getUploadpath(), vo.getUuid() + "_" + vo.getFilename());
+			checkFilePath.add(path);
+		});		
+			//썸네일 이미지
+		fileList.forEach(vo -> {
+			Path path = Paths.get("C:\\upload", vo.getUploadpath(), "s_" +  vo.getUuid() + "_" + vo.getFilename());
+			checkFilePath.add(path);
+		});
+		
+		
+		// 디렉토리 파일 리스트
+		File targetDir = Paths.get("C:\\upload", getFolderYesterDay()).toFile();
+		File[] targetFile = targetDir.listFiles();
+		
+		
+		// 삭제 대상 파일 리스트(분류)
+		List<File> removeFileList = new ArrayList<File>(Arrays.asList(targetFile));		
+		for(File file : targetFile){
+			checkFilePath.forEach(checkFile ->{
+				if(file.toPath().equals(checkFile)) 
+					removeFileList.remove(file);	
+			});
+		}
+		
+		
+		// 삭제 대상 파일 제거
+		log.warn("file Delete : ");
+		for(File file : removeFileList) {
+			log.info("File : {}",file);
+			file.delete();
+		}		
+		
+		log.warn("========================================");
+		
+	}
+}
